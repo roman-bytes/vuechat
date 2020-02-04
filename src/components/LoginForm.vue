3de39f012e0b32135d04d593342931fef2ccdbb0
Jacob Roman <jcbrmn06@gmail.com>
2020-02-04 13:40:41 -0500

    log in validation and redirected to chat dashboard

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   name: "login-form",
@@ -49,6 +49,15 @@ export default {
     },
     ...mapState(["loading", "error"]),
     ...mapGetters(["hasError"])
+  },
+  methods: {
+    ...mapActions(["login"]),
+    async onSubmit() {
+      const result = await this.login(this.userId);
+      if (result) {
+        this.$router.push("chat");
+      }
+    }
   }
 };
 </script>
